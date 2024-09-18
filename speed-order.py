369d5485a43da9a4d5a282006c445a22cf510c67
Ahmar Jenkins <104280394+Jstarri@users.noreply.github.com>
2024-09-18 18:04:52 -0400

    TIme

--- speed-order.py
+++ speed-order.py
@@ -1,3 +1,4 @@
+import time
 name_array = ["Greninja", "Mewtwo", "M_Rayquaza", "Seperior", "M_Lucario", "Lugia", "Zoroark", "Garchomp", "M_Charizard_X", "M_Gardevoir", "Charizard", "Typhlosion", "Xerneas", "Yveltal", "Mimikyu", "Kyurem", "Rayquaza", "M_Garchomp", "Giratina", "Lucario", "Suicune", "Arbok", "Chandelure", "Gardevoir", "Empoleon", "Torterra", "M_Mawile", "Mawile", "Golisopod"]
 stat_array = [132, 130, 115, 113, 112, 110, 105, 102, 100, 100, 100, 100, 99, 99, 96, 95, 95, 92, 90, 90, 85, 80, 80, 80, 60, 56, 50, 50, 40]
 
@@ -19,4 +20,5 @@ while True:
             continue
       if next_calculation == 'no':
             print("Goodbye!")
+            time.sleep(1)
             break
