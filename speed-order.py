e0320c6cec78faaf8d623cccc4e38a72126e08a9
Ahmar Jenkins <104280394+Jstarri@users.noreply.github.com>
2024-09-22 13:23:14 -0400

    Coin Flip Tidy Up

--- speed-order.py
+++ speed-order.py
@@ -4,7 +4,6 @@ import random
 name_array = ["Greninja", "Mewtwo", "M_Rayquaza", "Seperior", "M_Lucario", "Lugia", "Zoroark", "Garchomp", "M_Charizard_X", "M_Gardevoir", "Charizard", "Typhlosion", "Xerneas", "Yveltal", "Mimikyu", "Kyurem", "Rayquaza", "M_Garchomp", "Giratina", "Lucario", "Suicune", "Arbok", "Chandelure", "Gardevoir", "Empoleon", "Torterra", "M_Mawile", "Mawile", "Golisopod"]
 stat_array = [132, 130, 115, 113, 112, 110, 105, 102, 100, 100, 100, 100, 99, 99, 96, 95, 95, 92, 90, 90, 85, 80, 80, 80, 60, 56, 50, 50, 40]
 
-
 print("Welcome to the Pokemon VRC Speed Order!")
 while True:
       pokemon_amount = input("Enter the amount of pokemon: 2 or 4: ")
@@ -41,8 +40,12 @@ while True:
                   print(input_1, "is slower than", input_2, "!")
             elif stat_array[name_array.index(input_1)] == stat_array[name_array.index(input_2)]:
                   while stat_array[name_array.index(input_1)] == stat_array[name_array.index(input_2)]:
-                        print(input_1, "is the same speed as", input_2, "! Time for a coin flip! 1 is Heads & 2 is Tails!")
-                        print(random.randint(1,2))
+                        print(input_1, "is the same speed as", input_2, "! Time for a coin flip!")
+                        coin_flip = random.randint(1,2)
+                        if coin_flip == 1:
+                              print("Heads!")
+                        elif coin_flip == 2:
+                              print("Tails!")
                         next_loop = input("Do you need another coin flip? (yes/no): ")
                         if next_loop == 'yes':
                               continue
